c85172f602b8401168c409dd35a264718f65f93f
Peter Dresslar <peterdresslar@users.noreply.github.com>
2025-03-02 09:29:15 -1000

    a utility

--- model/llms/claude.py
+++ model/llms/claude.py
@@ -18,9 +18,6 @@ client = Anthropic(
 )
 
 
-
-
-
 def call_llm(model: str, prompt: str) -> str:
     """
     Call the LLM with the given prompt.
@@ -28,11 +25,11 @@ def call_llm(model: str, prompt: str) -> str:
     message = client.messages.create(
         max_tokens=1024,
         messages=[
-        {
-            "role": "user",
-            "content": prompt,
-        }
-    ],
+            {
+                "role": "user",
+                "content": prompt,
+            }
+        ],
         model=model,
     )
     return message.content
